40a5bce84c9994149b14a71d6a4adc93d80149a9
Oussama <bz.oussama@gmail.com>
2025-04-14 22:21:37 +0400

    add VehicleAnalytics

--- FMS_API/src/modules/metrics/metrics.types.ts
+++ FMS_API/src/modules/metrics/metrics.types.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 // Expecting payload to be dynamic JSON
 export const createMetricsSchema = z.object({
+  sensorId: z.string().min(1),
   payload: z.record(z.any()),
+  timestamp: z.string().datetime({ message: "Invalid ISO date format" }).default(new Date().toISOString()),
 });
 
 export type CreateMetricDTO = z.infer<typeof createMetricsSchema>;
