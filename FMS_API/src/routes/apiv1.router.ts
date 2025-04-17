77c2e8e32dd9c1b089b21110c1287ec6f4d1a06c
Oussama <bz.oussama@gmail.com>
2025-04-17 23:39:35 +0400

    remove generated prisma locally (not cross platform)

--- FMS_API/src/routes/apiv1.router.ts
+++ FMS_API/src/routes/apiv1.router.ts
@@ -8,7 +8,7 @@ apiv1router.use("/vehicles", vehicleRouter);
 apiv1router.use("/telemetry", telemetryRouter);
 
 // Swagger only in development (also servers at catch all for /api/v1)
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "development") {
   import("../config/swagger").then(({ swaggerUi, swaggerSpec }) => {
     apiv1router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     console.log(`Swagger docs: http://localhost:${process.env.PORT}/api/v1`);
